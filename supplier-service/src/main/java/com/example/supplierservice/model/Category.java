6fd6fd652f41d2b13b3106e031563eb805345800
Roman <ashsha51@mail.ru>
2024-03-10 04:53:22 +0300

    <commit message>

---
+++ supplier-service/src/main/java/com/example/supplierservice/model/Category.java
@@ -0,0 +1,25 @@
+package com.example.supplierservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @OneToMany(mappedBy="category")
+    private Set<Product> items;
+}
---
+++ supplier-service/src/main/java/com/example/supplierservice/model/Product.java
@@ -0,0 +1,29 @@
+package com.example.supplierservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    private String description;
+    @Column(nullable = false)
+    private BigDecimal price;
+    @ManyToOne(fetch = FetchType.LAZY,cascade = {CascadeType.PERSIST,CascadeType.MERGE})
+    private Category category;
+}
